64c9028e66ebc02a980ea5160e6b4116dc5c5c66
yuto1009 <bmwb71009@gmail.com>
2023-12-27 23:27:26 +0900

    add: basic laravel template

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+});
+
+Route::get('hello/{msg?}', function ($msg="no messages") {
+    $html = <<<EOF
+    <html>
+        <head>
+            <title>Hello</title>
+            <style>
+                body {font-size:16pt; color:#999;}
+                h1 {font-size:100pt; text-align:right; color:#eee;
+                    margin:-40px 0px -50px 0px; }
+            </style>
+        </head>
+        <body>
+            <h1>Hello</h1>
+            <p>{$msg}</p>
+            <p>This is sample page</p>
+        </body>
+    </html>
+    EOF;
+    return $html;
 });
 
 Route::get('/dashboard', function () {
