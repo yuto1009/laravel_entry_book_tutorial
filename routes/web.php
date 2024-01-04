35fe7efb9d9ddab60930d3b7cc7edf92c7936a98
yuto1009 <bmwb71009@gmail.com>
2024-01-04 14:16:55 +0900

    add: HelloController

--- routes/web.php
+++ routes/web.php
@@ -19,26 +19,8 @@ Route::get('/', function () {
 
 });
 
-Route::get('hello/{msg?}', function ($msg="no messages") {
-    $html = <<<EOF
-    <html>
-        <head>
-            <title>Hello</title>
-            <style>
-                body {font-size:16pt; color:#999;}
-                h1 {font-size:100pt; text-align:right; color:#eee;
-                    margin:-40px 0px -50px 0px; }
-            </style>
-        </head>
-        <body>
-            <h1>Hello</h1>
-            <p>{$msg}</p>
-            <p>This is sample page</p>
-        </body>
-    </html>
-    EOF;
-    return $html;
-});
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello/other', 'App\Http\Controllers\HelloController@other');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
