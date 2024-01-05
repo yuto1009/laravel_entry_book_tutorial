b00b665a55081b0ac0686873accea7a93bd729e0
yuto1009 <bmwb71009@gmail.com>
2024-01-05 16:23:51 +0900

    pend: try query literal

---
+++ resources/views/hello/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Hello/Index</title>
+        <style>
+            body {font-size:16pt; color:#999;}
+            h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+        </style>
+    </head>
+    <body>
+        <h1>Index</h1>
+        <p><?php echo $msg; ?></p>
+        <p>ID=<?php echo $id; ?></p>
+    </body>
+</html>
