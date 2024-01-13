28cbdab1c92781f69a734a81d6f00417f154802e
yuto1009 <bmwb71009@gmail.com>
2024-01-13 22:09:29 +0900

    add: how to use @each directive

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,11 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = ['one', 'two', 'three', 'four', 'five'];
+        $data = [
+            ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
+            ['name'=>'田中はなこ', 'mail'=>'hanako@tanaka'],
+            ['name'=>'鈴木さちこ', 'mail'=>'sachiko@suzuki']
+        ];
         return view('hello.index', ['data'=>$data]);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,15 +9,9 @@
 @section('content')
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます</p>
-    @component('components.message')
-        @slot('msg_title')
-        CAUTION!
-        @endslot
-
-        @slot('msg_content')
-        これはメッセージの表示です。
-        @endslot
-    @endcomponent
+    <ul>
+        @each('components.item', $data, 'item')
+    </ul>
 @endsection
 
 @section('footer')
